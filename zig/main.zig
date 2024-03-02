77c3741b1c48923a0b68e236781bbd89a06a53ae
Alhan Keser <alhankeser@gmail.com>
2024-03-02 15:57:09 +0100

    wip latest semi-working version still having issue with overflow

---
+++ zig/main.zig
@@ -0,0 +1,3 @@
+const user = @import("models/user.zig");
+const User = user.User;
+const MAX_POWER = user.MAX_POWER;
--- zig/merge_sort.zig
+++ zig/merge_sort.zig
@@ -8,13 +8,12 @@ fn mergeArrays(arr1: []u32, arr2: []u32, allocator: std.mem.Allocator) ![]u32 {
     var i: u32 = 0;
     var j: u32 = 0;
     while (i < arr1.len and j < arr2.len) {
-        print("mergeArrays arr1: {d}\n", .{arr1});
-        print("mergeArrays arr2: {d}\n", .{arr2});
+        // print("mergeArrays arr1: {d}\n", .{arr1});
+        // print("mergeArrays arr2: {d}\n", .{arr2});
         if (i < arr1.len and arr1[i] <= arr2[j]) {
             try result.append(arr1[i]);
             i += 1;
-        }
-        else {
+        } else {
             try result.append(arr2[j]);
             j += 1;
         }
@@ -25,22 +24,21 @@ fn mergeArrays(arr1: []u32, arr2: []u32, allocator: std.mem.Allocator) ![]u32 {
     for (arr2[j..]) |a2| {
         try result.append(a2);
     }
-    print("mergeArrays result: {d}\n", .{result.items});
+    // print("mergeArrays result: {d}\n", .{result.items});
     return result.items[0..];
 }
-
 
 fn mergeSort(arr: []u32, allocator: std.mem.Allocator) ![]u32 {
     if (arr.len <= 1) {
         return arr;
     }
     const midpoint = arr.len / 2;
-    var arr1 = arr[0..midpoint];
-    var arr2 = arr[midpoint..];
-    arr1 = try mergeSort(arr1,allocator);
-    arr2 = try mergeSort(arr2, allocator);
+    print("{d}\n", .{arr});
+    print("{d}\n", .{midpoint});
+    const arr1 = try mergeSort(arr[0..midpoint], allocator);
+    const arr2 = try mergeSort(arr[midpoint..], allocator);
     const result = try mergeArrays(arr1, arr2, allocator);
-    print("mergeSort result:{any}\n", .{result});
+    // print("mergeSort result:{d}\n", .{result[0]});
     return result;
 }
 
@@ -60,19 +58,19 @@ pub fn main() !void {
 
     var iterator = std.mem.split(u8, read_buf, "\n");
 
-    var input_array = std.ArrayList(u32).init(allocator);
-    defer input_array.deinit();
+    var inputArray = std.ArrayList(u32).init(allocator);
+    defer inputArray.deinit();
 
     while (iterator.next()) |raw| {
         const n: u32 = try std.fmt.parseInt(u32, raw, 10);
 
         if (@TypeOf(n) == u32) {
-            try input_array.append(n);
+            try inputArray.append(n);
         } else {
             // print("Error\n", .{});
         }
     }
 
-    const output_array = try mergeSort(input_array.items[0..], allocator);
-    print("{d}\n", .{output_array});
+    const outputArray = try mergeSort(inputArray.items[0..], allocator);
+    print("{d}\n", .{outputArray});
 }
